a2801e205a8d2c8c505f3a2333826b24ddd7b429
ottersec <121310374+ottersek@users.noreply.github.com>
2024-06-09 01:32:29 +0200

    <3
    <3

---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'kl-scripts'
+description 'Advanced NPC manager for FiveM'
+version '1.0.0'
+
+client_scripts {
+    'config.lua',
+    'client.lua'
+}
+
